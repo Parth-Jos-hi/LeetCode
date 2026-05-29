2d0720c59b78ee6397b8fad669671673ef5aaf0e
Parth Joshi <joshiparth936@gmail.com>
2026-05-29 06:14:02 +0530

    LC-maximum-sum-of-distinct-subarrays-with-length-k [Runtime: 99 ms; Memory: 98.3 MB]

--- hash-table/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ hash-table/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -2,39 +2,23 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int,int>freq;
-        int i ,j=0;
+        unordered_map<int,int> freq;
         long long sum = 0;
-        long long MAX =INT_MIN;
-        int curr_sum = 0;
-        for(int i =0;i<k;i++){
-            freq[nums[i]]++;
-            sum = sum+nums[i];
-            if(freq[nums[i]]>1){
-                curr_sum=0;
-            }
-            else{
-            curr_sum = sum;
-            }
-        }     
-        if(curr_sum!=sum) {
-            MAX = 0;
-        }   
-        else{
-        MAX  = max(MAX,sum);
-        }
-        i=0;
-        for(int j=k;j<n;j++){
+        long long MAX = 0;
+        int i = 0;
+        for(int j = 0; j < n; j++) {
             freq[nums[j]]++;
-            freq[nums[i]]--;
-            sum = sum+nums[j];
-            sum = sum-nums[i];
-            i++;
-            if(freq[nums[j]]>1||freq[nums[i]]>1){    
-                continue;
+            sum += nums[j];
+            while(freq[nums[j]] > 1 || j - i + 1 > k) {
+                freq[nums[i]]--;
+                sum -= nums[i];
+                i++;
             }
-            MAX  = max(MAX,sum);             
+            if(j - i + 1 == k) {
+                MAX = max(MAX, sum);
+            }
         }
         return MAX;
     }
 };
+
